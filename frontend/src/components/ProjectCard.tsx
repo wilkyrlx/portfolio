de4704fd2803ad7f5f45561b42234b9b1a7da6fa
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-05-17 02:04:36 -0400

    feat: close overlay button

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -14,9 +14,9 @@ function openProject(projectId: number) {
 
 function ProjectCard({ ...project }: IProject) {
     return (
-        <div className="card" key={project.id} onClick={event => openProject(project.id)}>
+        <div className="card project-box" key={project.id} onClick={event => openProject(project.id)}>
             <div className="tab-header">
-                <Image src="/images/sports.jpg" alt="" width={100} height={100} />
+                <Image src={"/images/" + project.img} alt="" width={100} height={100} />
             </div>
                 <h2 className="ml-10 text-xl font-bold text-gray-800">{project.name}</h2>
                 <p className="ml-10 text-gray-500">{project.description}</p>           
--- frontend/src/components/ProjectOverlay.tsx
+++ frontend/src/components/ProjectOverlay.tsx
@@ -2,15 +2,29 @@ import IProject from "@/types/IProject";
 import projects from '../data/projects.json' assert { type: 'json' };
 import Image from 'next/image';
 
+
+function closeOverlay(projectId: number) {
+    console.log("Closing project");
+
+    // unblur the entire background
+    const mainContent: HTMLElement = document.querySelector('#main-content') as HTMLElement;
+    const overlay: HTMLElement = document.querySelector('#project-overlay-' + projectId.toString()) as HTMLElement;
+    mainContent.classList.remove('blur');
+    overlay.classList.add('collapse');
+}
+
 /**
  * Opens the project overlay. This function is called when a project card is clicked.
  * Initially, the overlay is collapsed. When the overlay is expanded, the entire background is blurred.
  */
 function ProjectOverlay({ ...project }: IProject) {
     return (
-        <div className="overlay collapse" key={project.id} id={"project-overlay-" + project.id}>
+        <div className="overlay project-box collapse" key={project.id} id={"project-overlay-" + project.id}>
             <div className="tab-header">
-                <Image src="/images/sports.jpg" alt="" width={100} height={100} />
+                <Image src={"/images/" + project.img} alt="" width={100} height={100} />
+                <div className="button" onClick={event => closeOverlay(project.id)}>
+                    <Image src="/icons/close.svg" alt="Close" width={20} height={20} className="button-img" />
+                </div>
             </div>
             <h2 className="ml-10 text-xl font-bold text-gray-800">{project.name}</h2>
             <p className="ml-10 text-gray-500">{project.description}</p>
