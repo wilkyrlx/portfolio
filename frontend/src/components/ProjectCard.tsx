17c3c78d10db419b80c8151bd8937b17b1788b19
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-05-17 00:16:04 -0400

    style: 3-per-row cards, hover

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -2,19 +2,16 @@ import IProject from "../types/IProject";
 import Image from 'next/image';
 
 
-function ProjectTab({ ...project }: IProject) {
+function ProjectCard({ ...project }: IProject) {
     return (
         <div className="card" key={project.id}>
             <div className="tab-header">
                 <Image src="/images/sports.jpg" alt="" width={100} height={100} />
             </div>
-                <h2>{project.name}</h2>
-                <p>{project.description}</p>
-                <p>{project.githubUrl}</p>
-                <p>{project.demoUrl}</p>
-            
+                <h2 className="ml-10 text-xl font-bold text-gray-800">{project.name}</h2>
+                <p className="ml-10 text-gray-500">{project.description}</p>           
         </div>
     );
 }
 
-export default ProjectTab;
+export default ProjectCard;
--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,16 +1,16 @@
 import IProject from '@/types/IProject';
 import projects from '../data/projects.json' assert { type: 'json' };
-import ProjectTab from './ProjectTab';
+import ProjectCard from './ProjectCard';
 
 function Projects() {
     return (
         <div>
             <h1>Projects</h1>
-            <div className="projects grid gap-5 mx-auto md:grid-cols-2 lg:max-w-none">
+            <div className="projects grid gap-5 mx-auto md:grid-cols-3 lg:max-w-none">
                 {
                     projects.projects.map((projectData: IProject) => {
                         return (
-                            <ProjectTab {...projectData} />
+                            <ProjectCard {...projectData} />
                         )
                     })
                 }
