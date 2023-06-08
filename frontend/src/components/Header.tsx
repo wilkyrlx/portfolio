4df3ef35d92ea379466fc0ee20edbb718d187925
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-06-07 23:40:11 -0400

    header: resume

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import misc from '../data/misc.json' assert { type: 'json' };
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function Header() {
@@ -10,11 +12,15 @@ function Header() {
             <div className="space-x-1.5 flex items-center">
                 <a href={misc.links.github} className="text-lg w-14 h-14 rounded-full text-gray-800 transition-colors duration-150 hover:bg-gray-100 inline-flex
               items-center justify-center ring-blue-500 focus:ring-2 focus:outline-none">
-                    <FontAwesomeIcon icon={faGithub} style={{ color: "black" }} />
+                    <FontAwesomeIcon icon={faGithub} style={{ color: "black", width: 35 }} />
                 </a>
                 <a href={misc.links.linkedin} className="text-lg w-14 h-14 rounded-full text-gray-800 transition-colors duration-150 hover:bg-gray-100 inline-flex
               items-center justify-center ring-blue-500 focus:ring-2 focus:outline-none">
-                    <FontAwesomeIcon icon={faLinkedin} style={{ color: "black" }} />
+                    <FontAwesomeIcon icon={faLinkedin} style={{ color: "black", width: 35 }} />
+                </a>
+                <a href={misc.links.resume} className="text-lg w-14 h-14 rounded-full text-gray-800 transition-colors duration-150 hover:bg-gray-100 inline-flex
+              items-center justify-center ring-blue-500 focus:ring-2 focus:outline-none">
+                    <FontAwesomeIcon icon={faFile} style={{ color: "black", width: 25 }} />
                 </a>
             </div>
         </header>
